5a47b0a75c97e9c13a7b380672b04b8fa9a25d69
Alvin Yip <alvin.y97@gmail.com>
2024-10-05 21:21:57 +1000

    updated config to use psql and updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+const { sequelize } = require('./models');
+
 const app = express()
+
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 4000
 
@@ -11,3 +19,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
